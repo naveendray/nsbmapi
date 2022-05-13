22901d8038c9b1b6ef384ed43621c08eb6cd99ce
Yasitha Herath <yasithaherath@Yasithas-MacBook-Pro.local>
2022-05-13 19:15:25 +0530

    Dissable security

--- src/main/java/com/nsbm/studentapi/SecurityConfig.java
+++ src/main/java/com/nsbm/studentapi/SecurityConfig.java
@@ -1,31 +1,29 @@
-package com.nsbm.studentapi;
+// package com.nsbm.studentapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter
-{
-    @Override
-    protected void configure(HttpSecurity http) throws Exception
-    {
-        http
-                .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
-                .and()
-                .httpBasic();
-    }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception
-    {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}password").roles("ADMIN");
-    }
-}
+// @Configuration
+// public class SecurityConfig extends WebSecurityConfigurerAdapter
+// {
+//     @Override
+//     protected void configure(HttpSecurity http) throws Exception
+//     {
+//         http
+//                 .csrf().disable()
+//                 .authorizeRequests().anyRequest().authenticated()
+//                 .and()
+//                 .httpBasic();
+//     }
+
+//     @Autowired
+//     public void configureGlobal(AuthenticationManagerBuilder auth)
+//             throws Exception
+//     {
+//         auth.inMemoryAuthentication()
+//                 .withUser("user").password("{noop}password").roles("USER")
+//                 .and()
+//                 .withUser("admin").password("{noop}password").roles("ADMIN");
+//     }
+// }
